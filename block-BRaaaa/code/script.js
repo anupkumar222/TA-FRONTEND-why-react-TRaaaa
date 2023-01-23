1c7d5bc5fc4a6ee9a9e61c454dbe944961ebe598
anup kumar <kanup414@gmail.com>
2023-01-23 13:25:38 +0530

    done

--- block-BRaaaa/code/script.js
+++ block-BRaaaa/code/script.js
@@ -19,6 +19,32 @@ input.addEventListener('keyup', (event) => {
             watched : false,
         })
         event.target.value = '';
-        createMovieUI();
+        createMovieUI(allMovies, rootElm);
     }
 })
+
+function handleChange(event) {
+    let id = event.target.id;
+    allMovies[id].watched = !allMovies[id].watched;
+    createMovieUI(allMovies, rootElm);
+}
+
+function createMovieUI(data, root) {
+    root.innerHTML = '';
+    data.forEach((movie, i) => {
+        let li = document.createElement('li');
+        let button = document.createElement('button');
+        button.id = i;
+        button.innerText = movie.watched ? 'Watched' : 'To Watch';
+        button.addEventListener('click', handleChange);
+        let lable = document.createElement('label');
+        lable.for = i;
+        lable.innerText = movie.name;
+
+        li.append(lable, button);
+
+        rootElm.append(li);
+    })
+}
+
+createMovieUI(allMovies, rootElm);
